976a8d9d4009b2d1bef725b4721cc8049c658dd9
Artem Mishchenko <artemmishenko@mail.ru>
2021-03-31 23:45:05 +0400

    prod commit

--- ocr.py
+++ ocr.py
@@ -4,7 +4,7 @@ import cv2
 import os
 import numpy as np
 import sys
-import json
+from pdf2image import convert_from_path
 
 # Возвращаемый словарь
 result_dict = {
@@ -25,64 +25,12 @@ if __name__ == '__main__':
         pass
 
 
-def resize_image(percent, image):
-    # Ф-я изменения размера изображения
-    width = int(image.shape[1] * percent / 100)
-    height = int(image.shape[0] * percent / 100)
-    dim = (width, height)
-    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-    return resized
-
-
 def isolate_lines(src, structuring_element, kernel):
     # Ф-я изолирования линий таблицы
     cv2.erode(src, structuring_element, src, (-1, -1))
     cv2.dilate(src, structuring_element, src, (-1, -1))
     # Дополнительно удлиняем линии
     cv2.dilate(src, kernel, src, (-1, -1))
-
-
-def search_hsv_range():
-    # Ф-я поиска hsv диапазона
-    cv2.namedWindow("result")  # создаем главное окно
-    cv2.namedWindow("settings")  # создаем окно настроек
-
-    # создаем 6 бегунков для настройки начального и конечного цвета фильтра
-    cv2.createTrackbar('h1', 'settings', 0, 255, nothing)
-    cv2.createTrackbar('s1', 'settings', 0, 255, nothing)
-    cv2.createTrackbar('v1', 'settings', 0, 255, nothing)
-    cv2.createTrackbar('h2', 'settings', 255, 255, nothing)
-    cv2.createTrackbar('s2', 'settings', 255, 255, nothing)
-    cv2.createTrackbar('v2', 'settings', 255, 255, nothing)
-    crange = [0, 0, 0, 0, 0, 0]
-
-    while True:
-        img = cv2.imread('tmp/dataset_train/011_0e.png')
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-        # считываем значения бегунков
-        h1 = cv2.getTrackbarPos('h1', 'settings')
-        s1 = cv2.getTrackbarPos('s1', 'settings')
-        v1 = cv2.getTrackbarPos('v1', 'settings')
-        h2 = cv2.getTrackbarPos('h2', 'settings')
-        s2 = cv2.getTrackbarPos('s2', 'settings')
-        v2 = cv2.getTrackbarPos('v2', 'settings')
-
-        # формируем начальный и конечный цвет фильтра
-        h_min = np.array((h1, s1, v1), np.uint8)
-        h_max = np.array((h2, s2, v2), np.uint8)
-
-        # накладываем фильтр на кадр в модели HSV
-        thresh = cv2.inRange(hsv, h_min, h_max)
-
-        cv2.imshow('result', resize_image(40, thresh))
-
-        # ESC
-        ch = cv2.waitKey(5)
-        if ch == 27:
-            break
-
-    cv2.destroyAllWindows()
 
 
 def calc_red_areas_count(image: Image, isCalc=0):
@@ -128,20 +76,10 @@ def calc_red_areas_count(image: Image, isCalc=0):
     cv2.putText(closed, 'red: ' + str(object_count), (100, 100),
                 cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
 
-    # Задаем параметр resize для окна
-    # cv2.namedWindow("contours", cv2.WINDOW_NORMAL)
-    # cv2.namedWindow("closed", cv2.WINDOW_NORMAL)
-
-    # Выводим итоговое изображение в окно
-    # cv2.imshow('contours', resize_image(40, image))
-    # cv2.imshow('closed', resize_image(40, closed))
-
     # Записываем количество объектов
     if(isCalc):
         result_dict['red_areas_count'] = object_count
 
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
     return img_copy
 
 
@@ -200,59 +138,14 @@ def calc_blue_areas_count(image: Image, isCalc=0):
     cv2.putText(closed, 'blue: ' + str(object_count), (100, 100),
                 cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
 
-    # Задаем параметр resize для окна
-    # cv2.namedWindow("contours", cv2.WINDOW_NORMAL)
-    # cv2.namedWindow("closed", cv2.WINDOW_NORMAL)
-
-    # Выводим итоговое изображение в окно
-    # cv2.imshow('contours', resize_image(40, image))
-    # cv2.imshow('closed', resize_image(40, closed))
-
     # Записываем количество объектов
     if isCalc:
         result_dict['blue_areas_count'] = object_count
 
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
     return img_copy
 
 
-def find_areas():
-    image = cv2.imread('tmp/dataset_train/014_0e.png')
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (3, 3), 0)
-    thresh = cv2.threshold(
-        blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-    # Убрать внешнюю рамку
-    # cv2.floodFill(thresh, None, (0, 0), 255)
-    # cv2.floodFill(thresh, None, (0, 0), 0)
-
-    # Create rectangular structuring element and dilate
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (12, 12))
-    dilate = cv2.dilate(thresh, kernel, iterations=4)
-
-    # Find contours and draw rectangle
-    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    for c in cnts:
-        x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 2)
-    text = pytesseract.image_to_string(thresh, lang='rus')
-    print(text)
-    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-    # cv2.imshow('thresh', thresh)
-    # cv2.imshow('dilate', dilate)
-    cv2.imshow('image', resize_image(45, image))
-    cv2.waitKey()
-
-    pass
-
-
 def get_text_main_title(image: Image, isCalc=0):
-    # текст главного заголовка страницы или ""
-    # https://stackoverflow.com/questions/51933300/python-opencv-remove-border-from-image/51933482
-    # image = cv2.imread('tmp/dataset_train/006_0e.png')
     # Параметры картинки
     img_copy = image.copy()
     height, width, sl = image.shape
@@ -262,7 +155,6 @@ def get_text_main_title(image: Image, isCalc=0):
     blur = cv2.GaussianBlur(gray, (3, 3), 0)
     thresh = cv2.threshold(
         blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    # cv2.imshow('thresh', resize_image(45, thresh))
     # Убрать внешнюю рамку
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
     dilate = cv2.dilate(thresh, kernel, iterations=4)
@@ -303,28 +195,13 @@ def get_text_main_title(image: Image, isCalc=0):
             cv2.fillPoly(img_copy, pts=[contours], color=(255, 255, 255))
             pass
 
-    # Добавить проверку rect_points макс значение = ""
-    # Отрисовка прямоугольника
-    # cv2.rectangle(image, (rect_points[0], rect_points[1]),
-            # (rect_points[2], rect_points[3]), (36, 255, 12), 2)
-    # text = pytesseract.image_to_string(thresh, lang='rus')
     text = ""
     text = pytesseract.image_to_string(thresh[rect_points[1]:rect_points[3], rect_points[0] + 20:rect_points[2] - 20],
                                        config='--psm 13', lang='rus')
-    # print(text)
+    # Записываем результат
     if isCalc:
         result_dict['text_main_title'] = text[:-2]
-    # print(text)
-    # Изменить размер под экран
-    # cv2.namedWindow("thresh", cv2.WINDOW_NORMAL)
-    # cv2.namedWindow("dilate", cv2.WINDOW_NORMAL)
-    # cv2.namedWindow("im", cv2.WINDOW_NORMAL)
 
-    # cv2.imshow('thresh', resize_image(45, thresh))
-    # cv2.imshow('dilate', resize_image(45, dilate))
-    # cv2.imshow('image', resize_image(45, image))
-    # cv2.imshow('im', thresh[rect_points[1]:rect_points[3], rect_points[0] + 20:rect_points[2] - 20])
-    # cv2.waitKey()
     return img_copy
 
 
@@ -386,21 +263,6 @@ def get_first_text_block(image: Image, isCalc=0):
     else:
         text = ""
 
-    # print(text)
-    # rect_points = [0, 9999, 9999, 0, 9999]  # макс координаты
-    # point_accuracy = 500  # Допустимая погрешность в пикс.
-    # for c in cnts:
-    #     x, y, w, h = cv2.boundingRect(c)
-    #     wide = abs(x - (width - (x + w))) <= point_accuracy  # разница расстояний до краев листа
-    #     h_check = h > 25
-    #     if y < rect_points[1] and middle and h_check:
-    #         rect_points = [x, y, x + w, y + h]
-    #         # Закрасить заголовок на копии картинки
-    #         contours = np.array( [ [x,y], [x,y+h], [x+w,y+h], [x+w,y] ] )
-    #         mask = np.zeros((h + 2, w + 2), np.uint8)
-    #         cv2.fillPoly(img_copy, pts =[contours], color=(255,255,255))
-    #         pass
-    # print(text)
     res = text.split(' ')
     t = 0
     text = ""
@@ -410,30 +272,11 @@ def get_first_text_block(image: Image, isCalc=0):
     except IndexError:
         text = ""
 
-    # print(text)
+    # Записываем результат
     if isCalc:
         result_dict['text_block'] = text
+
     pass
-
-
-def sort_contours(cnts, method="left-to-right"):
-    # initialize the reverse flag and sort index
-    reverse = False
-    i = 0
-    # handle if we need to sort in reverse
-    if method == "right-to-left" or method == "bottom-to-top":
-        reverse = True
-    # handle if we are sorting against the y-coordinate rather than
-    # the x-coordinate of the bounding box
-    if method == "top-to-bottom" or method == "bottom-to-top":
-        i = 1
-    # construct the list of bounding boxes and sort them from top to
-    # bottom
-    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-                                        key=lambda b: b[1][i], reverse=reverse))
-    # return the list of sorted contours and bounding boxes
-    return (cnts, boundingBoxes)
 
 
 def calc_table_cells_count(image: Image, isCalc=0):
@@ -480,10 +323,6 @@ def calc_table_cells_count(image: Image, isCalc=0):
     # Находим ячейки
     intersections = cv2.bitwise_and(horizontal, vertical)
 
-    # Показываем маску
-    # cv2.imshow("mask", resize_image(45, mask))
-    # cv2.waitKey(0)
-
     cell_count = 0
 
     for j, cnt in enumerate(contours):
@@ -494,171 +333,41 @@ def calc_table_cells_count(image: Image, isCalc=0):
             con1 = np.array([[x, y], [x, y+h], [x+w, y+h], [x+w, y]])
             mask = np.zeros((h + 2, w + 2), np.uint8)
             cv2.fillPoly(img_copy, pts=[con1], color=(255, 255, 255))
-            # Считаем и показываем ячейки
-            # cv2.rectangle(image, (x, y), (x +
-            #                               w, y + h), (0, 0, 255), 3, 8, 0)
-            # cv2.putText(image, str(cell_count), (x, y + 25),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
-            # cv2.imshow("tables", resize_image(45, image))
-            # cv2.waitKey(0)
 
     # Записываем количество объектов
     if isCalc:
         result_dict['table_cells_count'] = cell_count
+
     return img_copy
 
 
 def extract_doc_features(filepath: str) -> dict:
-    """
-    Функция, которая будет вызвана для получения признаков документа, для которого
-   задан:
-    :param filepath: абсолютный путь до тестового файла на локальном компьютере (строго
-   pdf или png).
+    # Проверяем формат документа
+    if not filepath.endswith(".pdf") and not filepath.endswith(".png"):
+        print("Для запуска программы используйте только файлы формата pdf или png.")
+        sys.exit(1)
 
-    :return: возвращаемый словарь, совпадающий по составу и написанию ключей условию
-   задачи
-    """
-    # Загрузить образ
-    img = cv2.imread(filepath)
-    # gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Если pdf, конвертируем
+    if filepath.endswith(".pdf"):
+        ext_img = convert_from_path(filepath)[0]
+    else:
+        ext_img = Image.open(filepath)
 
+    # Сохраняем изображение
+    ext_img.save("target.png", "PNG")
+    img = cv2.imread("target.png")
+
+    # Заголовок текста
     get_text_main_title(calc_blue_areas_count(
         calc_red_areas_count(img.copy())), 1)
+    # Синие области
     calc_blue_areas_count(img.copy(), 1)
+    # Красные области
     calc_red_areas_count(img.copy(), 1)
+    # Первые 10 слов параграфа
     get_first_text_block(calc_table_cells_count(get_text_main_title(
         calc_blue_areas_count(calc_red_areas_count(img.copy())))), 1)
+    # Кол-во ячеек
     calc_table_cells_count(img.copy(), 1)
 
-    # Распознаем красные объекты
-    # img = calc_red_areas_count(image.copy())
-    # Распознаем синие объекты
-    # img = calc_blue_areas_count(img)
-    # Распознаем заголовок
-    # get_text_main_title(img.copy()) # Должен принимать изображение без печатей
-    # img = calc_table_cells_count(img)
-    # get_first_text_block(img)
     return result_dict
-
-
-# Вызываем функцию с аргументом абсолютного пути к файлу
-# Для подсчета печатей/подписей
-if sys.argv[1] == "1":
-    # extract_doc_features(sys.argv[1])
-    red_eq = 0
-    blue_eq = 0
-
-    for number in range(15):
-        number += 1
-        if number < 10:
-            number = '0' + str(number)
-        extract_doc_features('tmp/dataset_train/0' + str(number) + '_0e.png')
-
-        with open('tmp/validation_train/0' + str(number) + '_0e.json', 'r') as handle:
-            data = json.load(handle)
-            # Выводим
-            print('file_' + str(number))
-            print('red:', data['red_areas_count'],
-                  result_dict['red_areas_count'])
-            print('blue:', data['blue_areas_count'],
-                  result_dict['blue_areas_count'])
-            print('')
-            # Проверяем совпало ли
-            if data['red_areas_count'] == result_dict['red_areas_count']:
-                red_eq += 1
-            if data['blue_areas_count'] == result_dict['blue_areas_count']:
-                blue_eq += 1
-
-    print('red_eq:', str(red_eq) + '/15')
-    print('blue_eq:', str(blue_eq) + '/15')
-
-# Для вывода заголовка
-if sys.argv[1] == "2":
-    # get_first_text_block()
-    # get_text_main_title()
-    img = cv2.imread('tmp/dataset_train/006_0e.png')
-    # get_first_text_block(calc_table_cells_count(get_text_main_title(calc_blue_areas_count(calc_red_areas_count(img)))))
-    # find_areas()
-    get_text_main_title(img)
-
-# Для поиска цветового диапазона
-if sys.argv[1] == "color":
-    search_hsv_range()
-
-# Для поиска таблиц
-if sys.argv[1] == "3":
-    right_count = 0
-
-    for number in range(15):
-        number += 1
-        if number < 10:
-            number = '0' + str(number)
-        img = cv2.imread('tmp/dataset_train/0' + str(number) + '_0e.png')
-        calc_table_cells_count(img)
-
-        with open('tmp/validation_train/0' + str(number) + '_0e.json', 'r') as handle:
-            data = json.load(handle)
-            # Выводим
-            print('file_' + str(number))
-            print('right_count:', data['table_cells_count'],
-                  result_dict['table_cells_count'])
-            print('')
-            # Проверяем совпало ли
-            if data['table_cells_count'] == result_dict['table_cells_count']:
-                right_count += 1
-    print('right_count:', str(right_count) + '/15')
-    # calc_table_cells_count(get_text_main_title(
-    # calc_blue_areas_count(calc_red_areas_count(img))))
-
-    # cv2.namedWindow("image1", cv2.WINDOW_NORMAL)
-    # cv2.imshow('image1', resize_image(45, img1))
-    # cv2.waitKey()
-
-if sys.argv[1] == "res":
-    for number in range(15):
-        number += 1
-        if number < 10:
-            number = '0' + str(number)
-
-        result_count = 0
-
-        extract_doc_features('tmp/dataset_train/0' + str(number) + '_0e.png')
-        with open('tmp/validation_train/0' + str(number) + '_0e.json', 'r') as handle:
-            data = json.load(handle)
-            # Выводим
-            print('--------------------------------------------------------')
-            print('filename:', '0'+str(number)+'_0e.png')
-            print('red_areas_count:',
-                  result_dict['red_areas_count'], data['red_areas_count'])
-            print('blue_areas_count:',
-                  result_dict['blue_areas_count'], data['blue_areas_count'])
-            print('table_cells_count:',
-                  result_dict['table_cells_count'], data['table_cells_count'])
-            print('text_main_title:',
-                  result_dict['text_main_title'])
-            print('text_block:',
-                  result_dict['text_block'])
-            print('file_' + str(number))
-
-            # Проверяем сколько совпало
-            if data['red_areas_count'] == result_dict['red_areas_count']:
-                result_count += 1
-
-            # Проверяем сколько совпало
-            if data['blue_areas_count'] == result_dict['blue_areas_count']:
-                result_count += 1
-
-            # Проверяем сколько совпало
-            if data['table_cells_count'] == result_dict['table_cells_count']:
-                result_count += 1
-
-            # Проверяем сколько совпало
-            if data['text_main_title'] == result_dict['text_main_title']:
-                result_count += 1
-
-            # Проверяем сколько совпало
-            if data['text_block'] == result_dict['text_block']:
-                result_count += 1
-
-        print('result_count=', result_count, '/5')
-        result_count = 0
